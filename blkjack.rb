5c9509a5432c47e80de2971b6942d7354a311026
bklynate <nathanielcfa@gmail.com>
2015-02-16 18:10:10 -0500

    try_again bug

--- blkjack.rb
+++ blkjack.rb
@@ -39,101 +39,107 @@ def hand_value(cards)
   total
 end
 
+begin
+  # Game Start Logistics
 
-#Game Start Logistics
-suits = ["Clubs :","Diamonds :","Spades :","Hearts :"]
-card_values = [2,3,4,5,6,7,8,9,10,"King","Queen","Jack","Ace"]
+  # Deck defined
+  suits = ["Clubs :","Diamonds :","Spades :","Hearts :"]
+  card_values = [2,3,4,5,6,7,8,9,10,"King","Queen","Jack","Ace"]
 
-deck = suits.product(card_values)
-deck.shuffle!
+  deck = suits.product(card_values)
+  deck.shuffle!
 
-player_cards = []
-dealer_cards = []
+  player_cards = []
+  dealer_cards = []
 
-player_cards << deck.pop
-player_cards << deck.pop
-dealer_cards << deck.pop
-dealer_cards << deck.pop
-
-player_total = hand_value(player_cards)
-dealer_total = hand_value(dealer_cards)
-
-prompt "Welcome, what is your name?"
-
-player_name = gets.chomp.capitalize
-
-prompt "Welcome to Nate's Blackjack"
-
-
-prompt "#{player_name}'s cards are #{player_cards[0][0]} #{player_cards[0][1]} & #{player_cards[1][0]} #{player_cards[1][1]}"
-prompt "The value of the player's cards are #{player_total}"
-
-
-if player_total == 21
-  prompt "#{player_name} has hit Blackjack - You Win!!" 
-  exit
-end
-
-while player_total < 21
-  prompt "You may 'Hit' or 'Stay': Press 1 to 'Hit' Press 2 to 'Stay'" 
-  choice = user_input
-
-  if choice == '2'
-    prompt "#{player_name} chooses to stay."
-    break
-  end
-
-  new_card = deck.pop
-  prompt "#{player_name} was dealt #{new_card[0]} #{new_card[1]}"
-  player_cards << new_card
+  #Player cards dealt
+  player_cards << deck.pop
+  player_cards << deck.pop
+  dealer_cards << deck.pop
+  dealer_cards << deck.pop
 
   player_total = hand_value(player_cards)
-  prompt "The value of #{player_name}'s' cards are #{player_total}" 
+  dealer_total = hand_value(dealer_cards)
+
+  prompt "Welcome, what is your name?"
+
+  player_name = gets.chomp.capitalize
+
+  # Start of player turn
+
+  prompt "Welcome to Nate's Blackjack"
+  prompt "#{player_name}'s cards are #{player_cards[0][0]} #{player_cards[0][1]} & #{player_cards[1][0]} #{player_cards[1][1]}"
+  prompt "The value of the player's cards are #{player_total}"
 
   if player_total == 21
     prompt "#{player_name} has hit Blackjack - You Win!!" 
     exit
-  elsif player_total > 21
-    prompt "#{player_name} has busted!! - #{player_name} Loses!!" 
-    exit
   end
-end
+
+  while player_total < 21
+    prompt "You may 'Hit' or 'Stay': Press 1 to 'Hit' Press 2 to 'Stay'" 
+    choice = user_input
+
+    if choice == '2'
+      prompt "#{player_name} chooses to stay."
+      break
+    end
+
+    new_card = deck.pop
+    prompt "#{player_name} was dealt #{new_card[0]} #{new_card[1]}"
+    player_cards << new_card
+
+    player_total = hand_value(player_cards)
+    prompt "The value of #{player_name}'s' cards are #{player_total}" 
+
+    if player_total == 21
+      prompt "#{player_name} has hit Blackjack - You Win!!" 
+      exit
+    elsif player_total > 21
+      prompt "#{player_name} has busted!! - #{player_name} Loses!!" 
+      exit
+    end
+  end
 
 
-# Dealer's turn
-prompt "The Dealer's cards are #{dealer_cards[0][0]} #{dealer_cards[0][1]} & #{dealer_cards[1][0]} #{dealer_cards[1][1]}"
-prompt "The value of dealer's cards are #{dealer_total}"
+  # Dealer's turn
+  prompt "The Dealer's cards are #{dealer_cards[0][0]} #{dealer_cards[0][1]} & #{dealer_cards[1][0]} #{dealer_cards[1][1]}"
+  prompt "The value of dealer's cards are #{dealer_total}"
 
-if dealer_total == 21
-  prompt "Dealer has hit Blackjack - You Win!!" 
-  exit
-end
-
-while dealer_total < 17
-
-  new_card = deck.pop
-  prompt "Dealer was dealt #{new_card[0]} #{new_card[1]}"
-  dealer_cards << new_card
-
-  dealer_total = hand_value(dealer_cards)
-  prompt "The value of dealer's cards are #{dealer_total}" 
+  # Initial check for dealer blackjack
 
   if dealer_total == 21
-    prompt "Dealer has hit Blackjack - You Lose!!" 
-    exit
-  elsif dealer_total > 21
-    prompt "Dealer has busted!! - #{player_name} Wins!!" 
-    exit
+    prompt "Dealer has hit Blackjack - You Win!!" 
+    
   end
 
-end
+  while dealer_total < 17
+    # Dealer hit
 
-if dealer_total > player_total
-  prompt "Dealer Wins!!"
-elsif dealer_total < player_total 
-  prompt "#{player_name} Wins!!"
-else
-  prompt "It's a tie....."  
-end
+    new_card = deck.pop
+    prompt "Dealer was dealt #{new_card[0]} #{new_card[1]}"
+    dealer_cards << new_card
+    dealer_total = hand_value(dealer_cards)
+    prompt "The value of dealer's cards are #{dealer_total}" 
+    if dealer_total == 21
+      prompt "Dealer has hit Blackjack - You Lose!!" 
+      
+    elsif dealer_total > 21
+      prompt "Dealer has busted!! - #{player_name} Wins!!" 
+      
+    end
+  end
 
-exit
+# Compare Stays
+
+  if dealer_total > player_total
+    prompt "Dealer Wins!!"
+  elsif dealer_total < player_total 
+    prompt "#{player_name} Wins!!"
+  else
+    prompt "It's a tie....."  
+  end
+
+  prompt "Try again? [y/n]"
+  play_again = gets.chomp.downcase
+end unless play_again == "n"
